125951d4fb1ea8a01ae3bd084963c49951f64842
kamileb <56298422+kamileb@users.noreply.github.com>
2020-08-19 19:07:41 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// This is the wlcoming message for index page 
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// This is to specify the properties and add timimng
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  // This is for contact me form
+  // Adding pop up message
+  $(".menu-activator").on("click", function() {
+    $("body").toggleClass("menu-active");
+  });
